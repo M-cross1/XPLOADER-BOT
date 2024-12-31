43b3399fdb217c298560978cba5ad6c552d4e7ea
😈𝐃𝐄𝐌𝐎𝐍 𝐌-𝐂𝐑𝐎𝐒𝐒😈 <154022207+M-cross1@users.noreply.github.com>
2024-12-31 03:52:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,33 +15,33 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
 //=================================================//
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'XPLOADER~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYUw0SkVCTUdOcm92YUdvRzBReGRGKzNJYnpTU0ZGYkFna0JwSFMyVU0zbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiY2IzM2ZDd2FqTmJuekt3ak1tNG9uRWowaEtQNzg0dW9PZUlmeEZ6Z3pTRT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJLUGVjVmQ5eGxiZXAwNktQa0J2VUJoM2M0eDl5WVZ0QUpnejRkczBRUTFrPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI0Q0JYQnJtd0tKMWdxMXlGWFBxdHZCOFhIdXl5aXhoWnFHb2pVU0tVN2w0PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InlEWDhOQk11eE1DT1loZTNtV2RIS3BNbEYwL1l3b3k5em1rM2FZeWFsSGs9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Imp4MStIVTJnTlZUOU1JVHNWd0tRa09RRUFKcG1xV2h2aFJpWFRRSU5vWFE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOFBFUml4bTBBaVFvK3RvRi9IS3BLQkV0K0NuQlU0c0YxOUE2Z1d3S29sWT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiREZoVHpoVVVNY2QyNlFzRFRLaFNHQkY3eEE1a2wrMzFDMGVMaGZteGRtTT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjB0QUx4SEtFbngwK3JEcDh5ZU9sUzJrYzc1YVlMeC9rKzF6d1YzOGxnMGM4bUVrOUFOU2E5aWpwWERVMkVoOWZWZVVmWDh2T1B0U201VzFMREpDVWpnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6ODMsImFkdlNlY3JldEtleSI6IjhTTmRIelM4UUt6d3hNWXY1dkJEWTlmb1lVQTViSkJFTGpiWHoyRUV5MDQ9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbeyJrZXkiOnsicmVtb3RlSmlkIjoiMjM0OTA0MjU2NjQ3M0BzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiJEMzk1MjI1NDFEMTRDQzFBOEZFNzNFRDQ2MDREQUZEOSJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzM1NjEzMjg2fSx7ImtleSI6eyJyZW1vdGVKaWQiOiIyMzQ5MDQyNTY2NDczQHMud2hhdHNhcHAubmV0IiwiZnJvbU1lIjp0cnVlLCJpZCI6IjgxQzZFM0FBMkZGNjU1QzE3QjVDN0M5N0M5RDk3Q0YwIn0sIm1lc3NhZ2VUaW1lc3RhbXAiOjE3MzU2MTMyODl9XSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjEsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6IkNNdXR6SmdvUnYyN0ppbXpOblVuN2ciLCJwaG9uZUlkIjoiZmRlYTBiZWEtNzcyNi00MGZlLTlmZDgtNjUzNGU0MzJjZjAyIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFqQ2R6RWxJRHRmOTdudlloV2NCL01pVlYvVT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJnZkQwRDBFVGNhQ0s0Z1AzZHh5eGtFZ2tLTUU9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiRjI3WjRLUjMiLCJtZSI6eyJpZCI6IjIzNDkwNDI1NjY0NzM6MUBzLndoYXRzYXBwLm5ldCIsImxpZCI6IjIxMTA4OTYyMzA5NzUyMzoxQGxpZCIsIm5hbWUiOiLjhaTjhaTjhaTjhaQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0wzRTVOOEJFTksyemJzR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IlB6ZXRReVdVNEM3dHkwajFZYWhCRHpsVkZKbXVxaVdMNTZPQXp1a2dYVXM9IiwiYWNjb3VudFNpZ25hdHVyZSI6InBOVXdFak9wY3pNUitnaTNmTkxWQmRJU283WDVSR0VObkR2ekVOdVpLSWtPL0JjQ2oxNHJDdTIvc09GcDhEeTA0Z3R6bE5xWkJnTHZxNU9PRTMvK0R3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJwcDJxclpvN3Z1a0ZzOTVYWEZZQ214VmZhcFRVMVpBM20rRVprRjVWZHN4VmNMMVB6aGxONjJsejZRejgrQXduMnlaMVNQWm1abkhsQUtrZ1E0cXFpdz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzNDkwNDI1NjY0NzM6MUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJUODNyVU1sbE9BdTdjdEk5V0dvUVE4NVZSU1pycW9saStlamdNN3BJRjFMIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzM1NjEzMjgwLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUZVRyJ9' 
 //Enter your Xploader session id here; must start with XPLOADER~
 
 //=================================================//
-global.botname = process.env.BOT_NAME || '𝐗𝐩𝐥𝐨𝐚𝐝𝐞𝐫𝐁𝐨𝐭' 
+global.botname = process.env.BOT_NAME || 'M-CROSS' 
 //Your desired bot name
 
 //=================================================//
-global.ownernumber = process.env.OWNER_NUMBER || '254754783972' 
+global.ownernumber = process.env.OWNER_NUMBER || '2349042566473' 
 //Type your number here
 
 //=================================================//
-global.ownername = process.env.OWNER_NAME || 'Tylor' 
+global.ownername = process.env.OWNER_NAME || 'M-cross' 
 //Type your name here
 
 //=================================================//
 global.plink = process.env.PLINK || "https://www.instagram.com/heyits_tylor?igsh=YzljYTk1ODg3Zg=="
 
 //=================================================//
-global.wm = process.env.GL_WM || "©𝐗𝐩𝐥𝐨𝐚𝐝𝐞𝐫𝐁𝐨𝐭"
+global.wm = process.env.GL_WM || "©M-cross"
 
 //=================================================//
-global.packname = process.env.STICKER_PACK_NAME || "Xploader" 
+global.packname = process.env.STICKER_PACK_NAME || "Miracle" 
 //The sticker pack name
 
 //=================================================//
-global.author = process.env.STICKER_AUTHOR_NAME || "Bot" 
+global.author = process.env.STICKER_AUTHOR_NAME || "M-cross" 
 //The sticker author name
 
 //=================================================//
@@ -53,7 +53,7 @@ global.xprefix = process.env.PREFIX || '.'
 //Set your desired prefix
 
 //=================================================//
-global.mode = process.env.MODE || 'public';
+global.mode = process.env.MODE || 'private';
 // Set to 'private' to enable private mode, otherwise default is 'public'
 
 //=================================================//
@@ -71,15 +71,15 @@ global.anticall = process.env.ANTI_CALL || 'false'
 global.welcome = process.env.WELCOME_MSG || 'false'
 
 //=================================================//
-global.statusemoji = process.env.STATUS_EMOJI || '🙂'
+global.statusemoji = process.env.STATUS_EMOJI || '👻'
 //Set the emoji that you want to be reacted to status
 
 //=================================================//
-global.timezones = process.env.TIMEZONE || "Africa/Nairobi" 
+global.timezones = process.env.TIMEZONE || "Africa/Lagos" 
 //Don't edit this if you don't know!
 
 //=================================================//
-global.countrycode = process.env.COUNTRY_CODE || '254' 
+global.countrycode = process.env.COUNTRY_CODE || '234' 
 //set your country code for functionality of blockforeign and antiforeign commands
 
 //=================================================//
@@ -111,7 +111,7 @@ global.mess = {
   nsfw: '*Tell the admin to enable NSFW first!*', 
   done: '*Done*', 
   error: '*Sorry, I cannot perform that action!*', 
-  success: '*©𝐗𝐩𝐥𝐨𝐚𝐝𝐞𝐫𝐁𝐨𝐭*', 
+  success: '*© M-cross 𝐁𝐨𝐭*', 
   premium: '*Only premium users can use this command!*', 
   owner: '*Apologies, only my owner can use this command!*', 
   group: '*This feature becomes available when you use it in a group!*', 
